ea083c64f4452d2a757f224710641632a2aca82b
Tuan Giang <tuanng@heligate.com.vn>
2021-04-19 11:30:22 +0700

    Initialize

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let name;
+  export let age = 30;
+  function incrementAge() {
+    age++;
+  }
+</script>
+
+<h1>Hello {name}, {age}!</h1>
+<button on:click={incrementAge}>Change Age</button>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
