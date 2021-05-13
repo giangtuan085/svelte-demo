1aee60a569cb95b28619b4b09ae1fb5e8c4688c0
Tuan Giang <tuanng@heligate.com.vn>
2021-05-13 15:02:17 +0700

    Svelte basic syntax

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,40 @@
 <script>
+  import Contact from './Contact.svelte';
+
   export let name;
   export let age = 30;
+  let isBold;
+  $: uppercaseName = name.toUpperCase(); // label statement
   function incrementAge() {
     age++;
   }
+
+  function changeCss() {
+    isBold = !isBold;
+  }
+
+
 </script>
 
-<h1>Hello {name}, {age}!</h1>
+<h1 class={isBold ? 'bold-text' : 'normal-text'}>Hello {uppercaseName}, {age}!</h1>
 <button on:click={incrementAge}>Change Age</button>
+<button on:click={changeCss}>Change CSS</button>
+
+<input type="text" bind:value={name} />
+
+<Contact name={name}/>
+<!-- or just use {name} for short cut -->
 
 <style>
   h1 {
     color: purple;
+  }
+
+  .bold-text {
+    font-weight: 800;
+  }
+
+  .normal-text {
+    font-weight: normal;
   }
 </style>
---
+++ src/Contact.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let name;
+  $: uppercaseName = name.toUpperCase(); // label statement
+
+</script>
+
+<div class="container">
+    <header>
+        <h1>User:</h1><h2>{name}</h2>
+        <h2>Job Title</h2>
+    </header>
+</div>
+
+<style>
+  h1 {
+    color: grey;
+  }
+</style>
